f97aeeba545c7e2d762c1ae21ca03a3db259302a
Andrew Gnott <agnott@civisanalytics.com>
2018-05-18 17:38:44 -0500

    ENH: Exploratory code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,66 @@
 const seedrandom = require('seedrandom');
 // seedrandom('test', { global: true });
 
+const reducers = {
+  sum(acc, val) {
+    return acc + val;
+  },
+};
+
+const comparators = {
+  gt(c) {
+    return x => x > c;
+  },
+  lt(c) {
+    return x => x < c;
+  },
+  gteq(c) {
+    return x => x >= c;
+  },
+  lteq(c) {
+    return x => x <= c;
+  },
+  eq(c) {
+    return x => x == c;
+  },
+  between(a, b) {
+    return x => a < x && b > x;
+  },
+  betweenInclusive(a, b) {
+    return x => a <= x && b >= x;
+  },
+  outside(a, b) {
+    return x => a > x || b < x;
+  },
+  outsideInclusive(a, b) {
+    return x => a >= x || b <= x;
+  },
+  matches(regex) {
+    return x => x.match(regex);
+  },
+  // Composition helpers
+  not(fn) {
+    return x => !fn(x);
+  },
+  or(...args) {
+    switch (args.length) {
+      case 1:
+        return x => args[0](x);
+      default:
+        return x => args[0](x) || this.or(...(args.slice(1)))(x);
+    }
+  },
+  and(...args) {
+    switch (args.length) {
+      case 1:
+        return x => args[0](x);
+      default:
+        return x => args[0](x) && this.or(...(args.slice(1)))(x);
+    }
+  }
+};
+const comps = comparators;
+
 class Random {
   constructor() {
     this.defaults = {
@@ -8,7 +68,6 @@ class Random {
       max: 100,
       mode: 50,
     };
-
   }
 
   random() {
@@ -46,11 +105,134 @@ class Random {
   }
 }
 
-const r = new Random();
+const random = new Random();
 
-console.log(r.float());
-console.log(r.int());
-console.log(r.triangular());
-console.log(r.float.triangular(0, 10, 5));
-console.log(r.int.triangular(0, 10, 5));
-console.log(r.choice([45, 123, 2435, 1213, 523, 564]));
+class Attributes {
+  constructor() {
+    this.array = [
+      'speed',
+      'rebound',
+      'block',
+      'steal',
+      'three',
+      'mid',
+      'short',
+      'dunk',
+      'pass',
+      'stamina',
+    ];
+    this.object = {};
+    this.array.forEach((attr) => {
+      this.object[attr] = random.triangular(0, 100, 50);
+    });
+  }
+
+  overall() {
+    const v = Object.values(this.object);
+    const exp = 0.675;
+    const s = v.map((val) => val ** exp).reduce(reducers.sum);
+    return Math.min(99, s / ((100 ** exp) * v.length) * 100 + 15);
+  }
+
+  *[Symbol.iterator]() {
+    for (let pair of Object.entries(this.object)) yield pair;
+  }
+}
+
+class Personality {
+  constructor() {
+    this.array = ['clutchness', 'cooperativeness', 'expressiveness', 'greediness'];
+    this.object = {};
+    this.array.forEach((attr) => {
+      this.object[attr] = random.triangular(0, 100, 50);
+    });
+  }
+}
+
+class Name {
+  constructor() {
+    this.first = random.choice(['TIM', 'JIM', 'MARK', 'MIKE', 'TOM', 'JOHN']);
+    this.last = random.choice(['SMITH', 'JONES', 'JAMES', 'OCEAN', 'PITT', 'JACKSON']);
+  }
+  get full() {
+    return `${this.first} ${this.last}`;
+  }
+}
+
+class Player {
+  constructor() {
+    this.name = new Name();
+    this.attributes = new Attributes();
+    this.personality = new Personality();
+  }
+}
+
+const PlayersProxy = {
+  create(obj) {
+    return new Proxy(obj, this.handlers());
+  },
+  intersection(target) {
+    return (other, cmp) => {
+      return new Proxy(target.filter(v => !other.includes(v)), this.handlers());
+    };
+  },
+  union(target) {
+    return (other, cmp) => {
+      return new Proxy(target.filter(v => other.includes(v)), this.handlers());
+    };
+  },
+  query(target) {
+    return (acc, cmp) => new Proxy(target.filter(v => cmp(acc(v))), this.handlers());
+  },
+  sort(target) {
+    return (fn) => new Proxy(target.slice(0).sort(fn), this.handlers());
+  },
+  get(target, property) {
+    if (property === 'union') return this.union(target);
+    if (property === 'query') return this.query(target);
+    if (property === 'sort') return this.sort(target);
+    return target[property];
+  },
+  handlers() {
+    return {
+      get: this.get.bind(this),
+    };
+  }
+};
+
+class Team {
+  constructor() {
+    this.internal = { players: [] };
+    this.players = PlayersProxy.create(this.internal.players);
+  }
+}
+
+const t = new Team();
+
+for (let i = 0; i < 20; i++) t.players.push(new Player());
+//
+// const overalls = t.players.map(p => p.attributes.overall());
+// console.log(overalls) ;
+// const boundedOveralls = t.players
+//   .query(p => p.attributes.overall(), comps.not(comps.between(80, 85)))
+//   .map(p => p.attributes.overall());
+// console.log(boundedOveralls);
+
+const names = t.players.map(p => p.name.full);
+console.log(names);
+const chosen = t.players.query(p => p.name.full, comps.matches(/TOM/));
+const others = t.players.query(p => p.name.full, comps.matches(/SMITH/));
+console.log('---');
+console.log(chosen.union(others).map(p => p.name.full));
+
+// let p, o, v, avg = 0, count = 400;
+// const outputs = {};
+// for(let i = 0; i < count; i++) {
+//   p = new Player();
+//   o = Math.floor(p.attributes.overall());
+//   v = outputs[o];
+//   avg += o / count;
+//   outputs[o] = v ? v + 1 : 1;
+// }
+// console.log(outputs);
+// console.log(avg);
